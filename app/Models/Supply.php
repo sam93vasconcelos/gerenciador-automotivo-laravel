f7292da17fa2118fda3cb23d73c8ae5c017f648e
= <=>
2021-10-16 12:06:14 -0300

    Lidar com vírgulas e expiração do token

--- app/Models/Supply.php
+++ app/Models/Supply.php
@@ -17,4 +17,24 @@ class Supply extends Model
         'date', 
         'vehicle_id'
     ];
+
+    public function setKmAttribute($value)
+    {
+        $this->attributes['km'] = str_replace(',','.',$value);
+    }
+
+    public function setPriceAttribute($value)
+    {
+        $this->attributes['price'] = str_replace(',','.',$value);
+    }
+
+    public function setLitersAttribute($value)
+    {
+        $this->attributes['liters'] = str_replace(',','.',$value);
+    }
+
+    public function setTotalAttribute($value)
+    {
+        $this->attributes['total'] = str_replace(',','.',$value);
+    }
 }
